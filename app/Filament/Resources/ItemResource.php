50fb3c9a16208923ea76b0df01b5fbb850f2a259
0xyzone <58982560+0xyzone@users.noreply.github.com>
2025-06-19 23:33:42 +0545

    Update ItemResource.php

--- app/Filament/Resources/ItemResource.php
+++ app/Filament/Resources/ItemResource.php
@@ -170,8 +170,8 @@ class ItemResource extends Resource
     {
         return [
             'index' => Pages\ListItems::route('/'),
-            'create' => Pages\CreateItem::route('/create'),
-            'edit' => Pages\EditItem::route('/{record}/edit'),
+            // 'create' => Pages\CreateItem::route('/create'),
+            // 'edit' => Pages\EditItem::route('/{record}/edit'),
         ];
     }
 }
