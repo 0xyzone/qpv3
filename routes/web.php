cf72f1620c45c027af56c9620da4960422239e7b
0xyzone <58982560+0xyzone@users.noreply.github.com>
2025-11-16 21:10:51 +0545

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ Route::get('wifi', function () {
     return response('WIFI:T:WPA;P:9861748449;S:QuickPick Public;H:false;');
 })->name('wifi');
 
-Route::options('/{any}', function () {
-    return response('', 200)
-        ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'GET, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
-})->where('any', '.*');
+// Route::options('/{any}', function () {
+//     return response('', 200)
+//         ->header('Access-Control-Allow-Origin', '*')
+//         ->header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+//         ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+// })->where('any', '.*');
